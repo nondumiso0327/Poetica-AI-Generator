5238d27c63c802830b58452ee6714c699e35b956
nondumiso0327 <pamellanzungu@gmail.com>
2025-06-20 00:25:00 +0200

    updated my JS prompt

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
     let userInput=document.querySelector("#user-input")
     let apiKey = "0bb5f9a7fet7eb24e30d522obf46e44c";
     let prompt = `Instructions are Generatea poem about ${userInput.value}`;
-    let context = "you are a romantic poem expect and you love to write short poems and you must generate a four line poem and start on a New line for each sentence. Please make sure you follow these instructions";
+    let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
      
     console.log("Generating poem")
