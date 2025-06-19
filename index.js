609febdd59d840789bd0d12b777a15584de0a634
nondumiso0327 <pamellanzungu@gmail.com>
2025-06-20 00:19:26 +0200

    Added the axios inHTML and Added the api intergration

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
+    console.log(" Poem generated")
  new Typewriter("#poem", {
-  strings: "In quiet winds the soul can hear",
+  strings: response.data.answer,
   autoStart: true,
   Delay: 1,
   cursor: "",
-});
+});   
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+    let userInput=document.querySelector("#user-input")
+    let apiKey = "0bb5f9a7fet7eb24e30d522obf46e44c";
+    let prompt = `Instructions are Generatea poem about ${userInput.value}`;
+    let context = "you are a romantic poem expect and you love to write short poems and you must generate a four line poem and start on a New line for each sentence. Please make sure you follow these instructions";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+     
+    console.log("Generating poem")
+    console.log(`Prompt:${prompt}`)
+    console.log(`Context:${context}`)
+    axios.get(apiUrl).then(displayPoem);
+
+ 
 }
     
 let poemFormGenerator = document.querySelector("#point-generator");
